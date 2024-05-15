1a9be96c40d27a6c59f8b06b396701f682aa30ca
Vanessa Najjuuko <100261651+QueenVanessa7@users.noreply.github.com>
2024-05-15 22:24:49 +0300

    implement the Footer Component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,82 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer goes here</div>
+    <>
+    <section className='bg-[#6AA84F] py-10'>
+        <div className='max-w-[1100px] m-auto text-white' id='footer'>
+            <div className='flex justify-between'>
+                <div>
+                    <h1>Browse Our Site</h1>
+                    <ul>
+                    <li>Find a Lawyer</li>
+                    <li>Review Your Lawyer</li>
+                    <li>Legal Advice</li>
+                    <li>Recently Answered Questions</li>
+                    <li>Browse Practice Areas</li>
+                    <li>UbuntuAid Stories Blog</li>
+                    <li>For Lawyers</li>
+                    </ul>
+                </div>
+                <div>
+                    <h1>Browse Our Site</h1>
+                    <ul>
+                    <li>Find a Lawyer</li>
+                    <li>Review Your Lawyer</li>
+                    <li>Legal Advice</li>
+                    <li>Recently Answered Questions</li>
+                    <li>Browse Practice Areas</li>
+                    <li>UbuntuAid Stories Blog</li>
+                    <li>For Lawyers</li>
+                    </ul>
+                </div>
+                <div>
+                    <h1>Browse Our Site</h1>
+                    <ul>
+                    <li>Find a Lawyer</li>
+                    <li>Review Your Lawyer</li>
+                    <li>Legal Advice</li>
+                    <li>Recently Answered Questions</li>
+                    <li>Browse Practice Areas</li>
+                    <li>UbuntuAid Stories Blog</li>
+                    <li>For Lawyers</li>
+                    </ul>
+                </div>
+                <div>
+                    <h1>Browse Our Site</h1>
+                    <ul>
+                    <li>Find a Lawyer</li>
+                    <li>Review Your Lawyer</li>
+                    <li>Legal Advice</li>
+                    <li>Recently Answered Questions</li>
+                    <li>Browse Practice Areas</li>
+                    <li>UbuntuAid Stories Blog</li>
+                    <li>For Lawyers</li>
+                    </ul>
+                </div>
+                
+            </div>
+            <div className='flex justify-between py-10'>
+                <div>
+                    <ul className='flex justify-between gap-5'>
+                        <li>Terms of Use</li> |
+                        <li>Privacy Policy</li>|
+                        <li>Community Guidelines</li>
+                    </ul>
+                </div>
+                <div>
+                    <ul className='flex justify-between gap-5'>
+                    <li><img src="/src/assets/Facebook.svg" alt="UbuntuAid on Facebook" /></li>
+                    <li><img src="/src/assets/twitter.svg" alt="UbuntuAid on twitter" /></li>
+                    <li><img src="/src/assets/LinkedIn.svg" alt="UbuntuAid on LinkedIn" /></li>
+                    <li><img src="/src/assets/YouTube.svg" alt="UbuntuAid on YouTube" /></li>
+                </ul>
+                </div>
+            </div>
+            <p>© UbuntuAid. All Rights Reserved 2024</p>
+        </div>
+    </section>
+
+    </>
   )
 }
 
