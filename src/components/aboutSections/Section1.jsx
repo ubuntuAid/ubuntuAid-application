8986799d302d8d6786c741c126ae248e3b4dffdd
Masambu Emmanuel <masambue11@gmail.com>
2024-05-16 17:47:06 +0300

    Style the About us sections

--- src/components/aboutSections/Section1.jsx
+++ src/components/aboutSections/Section1.jsx
@@ -4,12 +4,12 @@
 function Section1() {
   return (
     <>
-       <section className="h-[100dvh] flex justify-start items-center">
-            <div className="bg-[#0A72BA] h-[100%] md:w-[800px] flex flex-col justify-center items-center">
-                <h1 className="md:text-7xl text-left font-bold text-white leading-10 px-24">Why <br/>UbuntuAid?</h1>
+       <section className="h-[100dvh] flex w-full">
+            <div className="bg-[#0A72BA] h-[100%] md:w-[50%] flex flex-col justify-center items-center">
+                <h1 className="md:text-7xl text-left font-bold text-white w-[50%]">Why <br/>UbuntuAid?</h1>
                 <p className="text-white text-left w-[50%] py-5">At UbuntuAid, we empower refugees by democratizing access to justice, placing the law's power directly into their hands.</p>
             </div>
-            <div id="aboutImage" className="h-[100%] md:w-[800px]">
+            <div id="aboutImage" className="h-[100%] md:w-[50%]">
             </div>
         </section> 
     </>
