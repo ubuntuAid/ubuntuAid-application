cb24c871d27e8600fb5e901306c0b551c0318e3d
Vanessa Najjuuko <100261651+QueenVanessa7@users.noreply.github.com>
2024-05-17 17:13:40 +0300

    style go back button and remember me button

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -14,7 +14,7 @@ function Login() {
             <div className='md:w-1/2 md:h-[100%] h-screen flex flex-col md:justify-around justify-evenly'>
                 <div className='flex justify-between items-center p-10'>
                     <Link to={'/'}><img src="/src/assets/ubuntuAid-logo.svg" alt="" /></Link>
-                    <div className='md:flex hidden text-[#0a72BA]'>
+                    <div className='md:flex gap-2 font-bold hidden text-[#0a72BA]'>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
                         </svg>
@@ -26,7 +26,7 @@ function Login() {
                     <form action="">
                         <label htmlFor="email">Enter your email to receive a one-time passcode</label><br />
                         <input type="email" name="email" id="email" placeholder='Email Address' className='w-full border-2 border-gray-500 rounded px-2 py-1 my-2' />
-                        <p><input type="checkbox" name="checkbox" id="checkbox" /><span>Remember me</span></p>
+                        <p className='flex items-center gap-2 py-1'><input type="checkbox" name="checkbox" id="checkbox" /><span className='text-sm'>Remember me</span></p>
                         <button type="submit" className='w-full bg-[#0a72ba] rounded p-2 my-2 text-center font-medium text-white'>Send 4-digit Code</button>
                     </form>
                     <div className='flex justify-between items-center my-2'><hr className='w-1/2'/><p className='px-4'>or</p><hr className='w-1/2' /></div>
