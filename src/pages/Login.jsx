a0e14c1e1f1608c634f6819afae841895959fe39
Vanessa Najjuuko <100261651+QueenVanessa7@users.noreply.github.com>
2024-05-17 17:07:14 +0300

    style for mobile layout

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,10 +11,10 @@ function Login() {
 
     return (
         <div className='md:flex md:h-screen justify-between items-center'>
-            <div className='md:w-1/2 h-[100%] flex flex-col justify-around'>
+            <div className='md:w-1/2 md:h-[100%] h-screen flex flex-col md:justify-around justify-evenly'>
                 <div className='flex justify-between items-center p-10'>
                     <Link to={'/'}><img src="/src/assets/ubuntuAid-logo.svg" alt="" /></Link>
-                    <div className='flex text-[#0a72BA]'>
+                    <div className='md:flex hidden text-[#0a72BA]'>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
                         </svg>
@@ -22,7 +22,7 @@ function Login() {
                     </div>
                 </div>
                 <div className='md:px-32 px-10'>
-                    <h1 className='md:text-4xl font-bold mb-2'>Sign in</h1>
+                    <h1 className='md:text-4xl text-2xl font-bold mb-2'>Sign in</h1>
                     <form action="">
                         <label htmlFor="email">Enter your email to receive a one-time passcode</label><br />
                         <input type="email" name="email" id="email" placeholder='Email Address' className='w-full border-2 border-gray-500 rounded px-2 py-1 my-2' />
