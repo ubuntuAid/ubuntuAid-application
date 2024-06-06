5482809c9a9cc6152afe11357e3716c8c9f216f3
Masambu Emmanuel <masambue11@gmail.com>
2024-06-06 15:21:38 +0300

    Edit the Home page, hero section title

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,11 @@
 /* eslint-disable react/no-unescaped-entities */
 import Footer from "../components/footer/Footer";
 import { Link } from "react-router-dom";
-import NavBar from "../components/navBar/NavBar"
+import NavBar from "../components/navBar/NavBar";
 import LawyerCarousel from "../components/carouselCard/LawyerCarousel";
 import { AuthProvider } from "../contexts/AuthContext"; // Ensure correct import path
 
-
 function HomePage() {
-  
   return (
     <AuthProvider>
       <NavBar />
@@ -23,29 +21,40 @@ function HomePage() {
               justice, placing the law's power directly into their hands.
             </p>
             <div className="md:flex md:gap-5">
-             <div className="py-5 md:py-0">
-             <Link to={'/login'} className="text-white bg-[#0072BC] px-2 py-2 rounded font-bold md:w-52 flex justify-center items-center">
-                GET HELP
-              </Link>
-             </div>
+              <div className="py-5 md:py-0">
+                <Link
+                  to={"/login"}
+                  className="text-white bg-[#0072BC] px-2 py-2 rounded font-bold md:w-52 flex justify-center items-center"
+                >
+                  GET HELP
+                </Link>
+              </div>
               <div>
-              <Link to={'/login'} className="text-white bg-[#6AA84F] px-2 py-2 rounded font-bold md:w-52 flex justify-center items-center">
-                OFFER AID
-              </Link>
+                <Link
+                  to={"/login"}
+                  className="text-white bg-[#6AA84F] px-2 py-2 rounded font-bold md:w-52 flex justify-center items-center"
+                >
+                  OFFER AID
+                </Link>
               </div>
             </div>
-          </div>
-          <div className="md:flex-none md:w-1/4">
-            {/* <h1>Just for styling</h1> */}
           </div>
         </div>
       </section>
       <section className="md:p-20 p-14 bg-[#0A72BA]">
-            <div className="md:max-w-[70%]] m-auto">
-            <h1 className="text-center md:text-6xl font-bold text-4xl"><span className="text-[#2e2e2e]">Ubuntu-Aid:</span> <span className="text-white">u·boon·too - Aid</span></h1>
-            <p className="text-center text-white md:px-56 md:py-8 py-5 text-2xl">Ubuntu; pronounced as u.boon.too simply means “Humanity to others”. UbuntuAid was coined to mean Aid to Humans specifically to the People who are displaced and seeking asylum. We are bringing the Ubuntu Spirit to the World of Refugees and Asylum Seekers</p>
-            </div>
-        </section>
+        <div className="md:max-w-[70%]] m-auto">
+          <h1 className="text-center md:text-6xl font-bold text-4xl">
+            <span className="text-[#2e2e2e]">Ubuntu-Aid:</span>{" "}
+            <span className="text-white">u·boon·too - Aid</span>
+          </h1>
+          <p className="text-center text-white md:px-56 md:py-8 py-5 text-2xl">
+            Ubuntu; pronounced as u.boon.too simply means “Humanity to others”.
+            UbuntuAid was coined to mean Aid to Humans specifically to the
+            People who are displaced and seeking asylum. We are bringing the
+            Ubuntu Spirit to the World of Refugees and Asylum Seekers
+          </p>
+        </div>
+      </section>
       <section className="md:py-40 py-14">
         <h1 className="text-center md:text-[3rem] text-3xl mb-10 md:px-0 px-10 font-bold text-[#6AA84D]">
           UbuntuAiders ready to support you!
